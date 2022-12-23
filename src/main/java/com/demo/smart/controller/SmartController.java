81c08b742a6acdd8252246f3269f0b0e7649ac3c
smanasawala52 <67199826+smanasawala52@users.noreply.github.com>
2022-12-23 18:14:39 +0530

    Update SmartController.java

--- src/main/java/com/demo/smart/controller/SmartController.java
+++ src/main/java/com/demo/smart/controller/SmartController.java
@@ -32,19 +32,32 @@ public class SmartController {
 		ModelAndView modelAndView = new ModelAndView("home");
 		return modelAndView;
 	}
-
+	
+// this methd is to load test data into the sytem
 	@GetMapping("/loadData")
 	public ModelAndView loadData(
 			@RequestParam final Map<String, String> queryParams) {
+		
+		// logic here is that we are going to have SmartModel which will have list of smartFeatures and also list of services and devices.
+		// Now as services and devices has common properties thus we are using CommonModel object for this purpose
+		// As you can see, this program requires SmartFeature also have list of services and devices, we are reusing same CommonModel to support
+				
 		ModelAndView modelAndView = new ModelAndView("home");
+		
+		// creating the instance of SmartModel
 		SmartModel smartModel = new SmartModel();
+		
+		// creating the instance of SmartModel devices and setting test value
 		List<CommonModel> devices = new ArrayList<>();
 		CommonModel device = new CommonModel("Remotely Controllable Camera",
 				"device", "smartModel");
 		devices.add(device);
+		// before setting it to smart model, we are saving it to DB
 		devices = commonModelRepository.saveAllAndFlush(devices);
 		smartModel.setDevices(devices);
+		//
 
+		// creating the instance of SmartModel services and setting test value
 		List<CommonModel> services = new ArrayList<>();
 		CommonModel service = new CommonModel("Open Weather Map", "service",
 				"smartModel");
@@ -52,13 +65,19 @@ public class SmartController {
 		service = new CommonModel("IMDB Movie Database", "service",
 				"smartModel");
 		services.add(service);
+		// before setting it to smart model, we are saving it to DB
 		services = commonModelRepository.saveAllAndFlush(services);
 		smartModel.setServices(services);
+		//
 
 		// smart features
+		// creating the instance of SmartFeatures and setting test value
 		List<SmartFeature> smartFeatures = new ArrayList<>();
 
+		
 		SmartFeature smartFeature = new SmartFeature();
+		
+		// creating the instance of SmartFeatures devices and setting test value
 		List<CommonModel> smartFeatureDevices = new ArrayList<>();
 		CommonModel smartFeatureDevice = new CommonModel(
 				"Take Camera Screenshot", "device", "smartFeature");
@@ -69,11 +88,13 @@ public class SmartController {
 		smartFeatureDevice = new CommonModel("Get Calories burned for a day",
 				"device", "smartFeature");
 		smartFeatureDevices.add(smartFeatureDevice);
-
+		// before setting it to smart features, we are saving it to DB
 		smartFeatureDevices = commonModelRepository
 				.saveAllAndFlush(smartFeatureDevices);
 		smartFeature.setDevices(smartFeatureDevices);
 
+
+		// creating the instance of SmartFeatures services and setting test value
 		List<CommonModel> smartFeatureServices = new ArrayList<>();
 		CommonModel smartFeatureService = new CommonModel(
 				"Get Weekly Forecast in a City", "service", "smartFeature");
@@ -86,14 +107,19 @@ public class SmartController {
 				"smartFeature");
 		smartFeatureServices.add(smartFeatureService);
 
+		// before setting it to smart features, we are saving it to DB
 		smartFeatureServices = commonModelRepository
 				.saveAllAndFlush(smartFeatureServices);
 		smartFeature.setServices(smartFeatureServices);
 
 		smartFeatures.add(smartFeature);
+		
+		// before setting it to smart model, we are saving it to DB
 		smartFeatures = smartFeatureRepository.saveAllAndFlush(smartFeatures);
 		smartModel.setSmartFeatures(smartFeatures);
+		//
 
+		// saving all info to DB
 		smartModelRepository.saveAndFlush(smartModel);
 
 		return modelAndView;
